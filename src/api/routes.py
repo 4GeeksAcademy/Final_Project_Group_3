0ef4ec04c13add3d73624f564a8d25414b32a942
Phoenix Reynolds <phoenix.b.reynolds@gmail.com>
2025-08-12 00:57:25 +0000

    wip

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,12 +5,16 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+
+app = Flask(__name__)
+jwt = JWTManager(app)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -36,3 +40,19 @@ def sign_up():
         return "recieved", 200
     else:
         return "Error, user could not be created", 500
+    
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    # Query your database for email and password
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        # The user was not found on the database
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+    # Create a new token with the user id inside
+    access_token = create_access_token(identity=user.email)
+    return jsonify({ "token": access_token, "user_id": user.id })
---
+++ src/front/pages/Login.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+    const [emailInputValue, setEmailInputValue] = useState("");
+    const [passwordInputValue, setPasswordInputValue] = useState("");
+
+    const login = async (email, password) => {
+        const resp = await fetch(`https://curly-space-doodle-v6wjv49jxxp62px57-3001.app.github.dev/api/token`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        })
+
+        if (!resp.ok) throw Error("There was a problem in the login request")
+
+        if (resp.status === 401) {
+            throw ("Invalid credentials")
+        }
+        else if (resp.status === 400) {
+            throw ("Invalid email or password format")
+        }
+        const data = await resp.json()
+        localStorage.setItem("jwt-token", data.token);
+
+        return data
+    }
+
+    return (
+        <div>
+            <div className="d-flex display-items-center justify-content-center">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-2"></div>
+                        <div className="col-8">
+                            <div className="border border-1 border-secondary rounded p-3 d-flex justify-content-center align-items-center" style={{ height: "70vh", boxSizing: "border-box" }}>
+                                <div>
+                                    <h1 className="mx-auto">Log in!</h1>
+                                    <div>
+                                        <input
+                                            className="form-control border-1 border-secondary focus-ring-0 m-1"
+                                            value={emailInputValue}
+                                            placeholder="email"
+                                            onChange={(event) => setEmailInputValue(event.target.value)}
+                                        />{/* onKeyDown={(event) => postTodo(event)} - change this to throw an error if both fields are not full */}
+                                        <input
+                                            className="form-control border-1 border-secondary focus-ring-0 m-1"
+                                            value={passwordInputValue}
+                                            placeholder="password"
+                                            onChange={(event) => setPasswordInputValue(event.target.value)}
+                                        />{/* onKeyDown={(event) => postTodo(event)} - change this to throw an error if both fields are not full */}
+                                        <Link to="/" className="btn btn-secondary ms-2 text-decoration-none">
+                                            Cancel
+                                        </Link>
+                                        <button type="text" onClick={() => login(emailInputValue, passwordInputValue)} className="btn btn-success ms-2 text-decoration-none">
+                                            Log in!
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
