5a20467546130e81490e66cc3a882745f08739e8
Phoenix Reynolds <phoenix.b.reynolds@gmail.com>
2025-09-12 21:58:20 +0000

    logo

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import SalonLogo from "../assets/img/SalonLogo.png";
 
 
 export const Navbar = () => {
@@ -14,8 +15,17 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-dark bg-dark">
 			<div className="container mb-1">
-				<Link to="/" className="text-decoration-none">
-					<span className="navbar-brand mb-0 h1 gold-title">Icon Nails & Spa</span>
+				<Link to="/" className="text-decoration-none d-flex align-items-center">
+					<div style={{ width: 55, height: 55 }}>
+						<img
+							src={SalonLogo}
+							alt="Logo"
+							style={{ width: "100%", height: "100%", objectFit: "contain" }}
+						/>
+					</div>
+					<span className="navbar-brand mb-0 h1 gold-title ms-2">
+						Icon Nails & Spa
+					</span>
 				</Link>
 				{/* Invisible div pushes all the buttons over */}
 				<div className="ms-auto"></div>
