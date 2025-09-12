f29185edc8ea2d80d3021c80b87d34fb93d34777
Phoenix Reynolds <phoenix.b.reynolds@gmail.com>
2025-09-12 21:25:20 +0000

    phoenix_branch

--- src/front/components/AccountHistory.jsx
+++ src/front/components/AccountHistory.jsx
@@ -1,8 +1,10 @@
 export const HistoryTab = () => {
 
     return (
-        <div>
-            history Tab
+        <div className="d-flex align-items-center justify-content-center">
+            <h1 className="text-secondary">
+            Coming soon!
+            </h1>
         </div>
     )
 }
